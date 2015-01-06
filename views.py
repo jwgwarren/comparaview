c848b003a7bb6c055ebb214a3fc6ada5a565415c
jwgwarren <warren.jonathan@gmail.com>
2015-01-06 15:33:35 +0000

    added a z stack veiw from example in docs

--- views.py
+++ views.py
@@ -1,7 +1,17 @@
-from django.http import HttpResponse
+from omeroweb.webclient.decorators import login_required
+from django.shortcuts import render_to_response
 
-def index(request):
-    """
-    Just a place-holder while we get started
-    """
-    return HttpResponse("Welcome to your app home-page2!")
+
+@login_required()
+def stack_preview (request, imageId, conn=None, **kwargs):
+    """ Shows a subset of Z-planes for an image """
+    image = conn.getObject("Image", imageId)       # Get Image from OMERO
+    image_name = image.getName()
+    sizeZ = image.getSizeZ()                        # get the Z size
+    # 5 Z-planes
+    z_indexes = [0, int(sizeZ*0.25),
+             int(sizeZ*0.5), int(sizeZ*0.75), sizeZ-1]
+    return render_to_response('webtest/stack_preview.html',
+         {'imageId':imageId,
+         'image_name':image_name,
+         'z_indexes':z_indexes})
