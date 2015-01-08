277b320f999750d83d4df57218a0ad615129e473
jwgwarren <warren.jonathan@gmail.com>
2015-01-08 16:41:26 +0000

    changed to full_viewer with no args but id

--- urls.py
+++ urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('django.views.generic.simple',
      # index 'home page' of the <your-app> app
      url( r'^$', views.index, name='comparaview_index' ),
      url( r'^compare/(?P<imageId>[0-9]+)/$', views.compare,
-     name="<your-app>_compare" ),
+     name="comparaview_compare" ),
 
  )
